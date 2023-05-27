cbdf7152ea93d941ea87dfebf94bd6a7e7d4815b
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-27 09:34:09 -0500

    feat: :sparkles: Third Exercise PHP
    performing the exercism "Resistor Color" exercise

--- api.php
+++ api.php
@@ -1,11 +1,17 @@
 <?php
+    declare(strict_types=1);
+    //ejemplo
+    $color = "black"
+    //ejemplo
+    const COLORS = [
+            "black", "brown", "red", "orange", "yellow",
+            "green", "blue", "violet", "grey", "white"
+        ];
 
-declare(strict_types=1);
-$text = "Hello I am a text string";
-function reverseString(string $text): string
-{
-    return strrev($text);
-}
-
-echo reverseString($text);
+    //el ejercicio obliga usar el const pero es mas recomendable usar el define()
+    function colorCode(string $color): int
+    {
+        $indice = array_search($color, COLORS);
+        return $indice;
+    }
 ?>
