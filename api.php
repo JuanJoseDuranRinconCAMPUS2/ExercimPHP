d49a0f4b059b179f9ffaa157ec29e191bd540a4f
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-27 09:13:07 -0500

    feat: :sparkles: Second Exercise PHP
    performing the exercism "Reverse String" exercise

--- api.php
+++ api.php
@@ -1,8 +1,11 @@
 <?php
-function HelloWorld(){
-    return "Hello, World!";
+
+declare(strict_types=1);
+$text = "Hello I am a text string";
+function reverseString(string $text): string
+{
+    return strrev($text);
 }
 
-
-echo HelloWorld();
+echo reverseString($text);
 ?>
