ef875c00dfffdc0823bd6826ac79294a99f34f28
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-28 16:52:50 -0500

    feat: :sparkles: Fourteenth Exercise PHP
    performing the exercism "Difference of Squares" exercise

--- api.php
+++ api.php
@@ -2,28 +2,20 @@
     //values for example
     declare(strict_types=1);
 
-    class Robot {
-        public $name;
-        private static $names = [];
+    function squareOfSum(int $max): int
+    {
+        $sum = $max * ($max +1)/ 2;
+        return $sum * $sum;
+    }
 
-        public function __construct() {
-            $this->reset();
-        }
+    function sumOfSquares(int $max): int
+    {
+        return $max * ($max + 1) * (2 * $max + 1)/ 6;
+    }
 
-        public function getName(): string {
-            return $this->name;
-        }
-
-        public function reset(): void {
-            $letters = range('A', 'Z');
-            $this->name = $letters[rand(0, 25)] . $letters[rand(0, 25)] . rand(100, 999);
-
-            if (in_array($this->name, self::$names)) {
-                $this->reset();
-            } else {
-                self::$names[] = $this->name;
-            }
-        }
+    function difference(int $max): int
+    {
+        return squareOfSum($max) - sumOfSquares($max);
     }
 
 ?>
