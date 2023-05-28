a9af9aae290a8c5da8ee63bfadd58f0751055fa9
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-28 14:40:00 -0500

    feat: :sparkles: Twelfth Exercise PHP
    performing the exercism "Isogram" exercise

--- api.php
+++ api.php
@@ -2,28 +2,11 @@
     //values for example
     declare(strict_types=1);
 
-    function isValid(string $number): bool
+    function isIsogram(string $word): bool
     {
-        $number = str_replace(' ', '', $number);
-
-        if (!ctype_digit($number) || empty($number)) {
-            return false;
-        }
-
-        $sum = 0;
-        $numDigits = strlen($number);
-
-        for ($i = $numDigits - 1; $i >= 0; $i--) {
-            $digit = (int)$number[$i];
-
-            if (($numDigits - $i) % 2 === 0) {
-                $digit *= 2;
-                $digit -= ($digit > 9) ? 9 : 0;
-            }
-
-            $sum += $digit;
-        }
-
-        return $sum % 10 === 0;
+        $word = str_replace([' ', '-'], '', $word);
+        $word = mb_strtolower($word, 'UTF-8');
+        return count(array_unique(mb_str_split($word))) === mb_strlen($word);
     }
+    
 ?>
