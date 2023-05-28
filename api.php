feef7244809e4f34b43caf4f02b7efe255251147
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-28 13:42:47 -0500

    feat: :sparkles: Tenth Exercises PHP
    performing the exercism "RNA Transcription" exercise

--- api.php
+++ api.php
@@ -1,33 +1,26 @@
 <?php
     //values for example
     declare(strict_types=1);
-    class Bob
-{
-    public function respondTo(string $str): string
-    {
-        $str = trim($str);
 
-        if (empty($str)) {
-            return "Fine. Be that way!";
-        } elseif ($this->isQuestion($str) && $this->isYelling($str)) {
-            return "Calm down, I know what I'm doing!";
-        } elseif ($this->isQuestion($str)) {
-            return "Sure.";
-        } elseif ($this->isYelling($str)) {
-            return "Whoa, chill out!";
+    function toRna(string $dna): string
+        {
+            $rna = '';
+            for ($i = 0; $i < strlen($dna); $i++) {
+                switch ($dna[$i]) {
+                    case 'G':
+                        $rna .= 'C';
+                        break;
+                    case 'C':
+                        $rna .= 'G';
+                        break;
+                    case 'T':
+                        $rna .= 'A';
+                        break;
+                    case 'A':
+                        $rna .= 'U';
+                        break;
+                }
+            }
+            return $rna;
         }
-
-        return "Whatever.";
-    }
-
-    private function isQuestion(string $str): bool
-    {
-        return substr($str, -1) === "?";
-    }
-
-    private function isYelling(string $str): bool
-    {
-        return $str === strtoupper($str) && preg_match("/[a-zA-Z]/", $str);
-    }
-}
 ?>
