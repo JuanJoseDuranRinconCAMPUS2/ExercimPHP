2781d3295b700ea63f836a22d6380dad961f6f66
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-28 18:07:51 -0500

    feat: :sparkles: Sixteenth Exercise PHP
    performing the exercism "Robot Simulator" exercise

--- api.php
+++ api.php
@@ -2,41 +2,86 @@
     //values for example
     declare(strict_types=1);
 
-    class School
-{
-    private $students = [];
-
-    public function numberOfStudents(): int {
-        return count($this->students);
-    }
-
-    public function add(string $name, int $grade): void {
-        $this->students[$name] = $grade;
-    }
-
-    public function grade(int $grade): array {
-        $result = [];
-        foreach ($this->students as $name => $studentGrade) {
-            if ($studentGrade === $grade) {
-                $result[] = $name;
+    class Robot
+    {
+        const DIRECTION_NORTH = 'NORTH';
+        const DIRECTION_EAST ='EAST';
+        const DIRECTION_SOUTH = 'SOUTH';
+        const DIRECTION_WEST = 'WEST';
+        /**
+         *
+         * @var int[]
+         */
+        public $position;
+        /**
+         *
+         * @var string
+         */
+        public $direction;
+    
+        public function __construct(array $position, string $direction) {
+            $this->position = $position;
+            $this->direction = $direction;
+        }
+    
+        public function turnRight(): self {
+            $directions = [
+                self::DIRECTION_NORTH => self::DIRECTION_EAST,
+                self::DIRECTION_EAST => self::DIRECTION_SOUTH,
+                self::DIRECTION_SOUTH => self::DIRECTION_WEST,
+                self::DIRECTION_WEST => self::DIRECTION_NORTH
+            ];
+            $this->direction = $directions[$this->direction];
+            return $this;
+        }
+    
+        public function turnLeft(): self {
+            $directions = [
+                self::DIRECTION_NORTH => self::DIRECTION_WEST,
+                self::DIRECTION_WEST => self::DIRECTION_SOUTH,
+                self::DIRECTION_SOUTH => self::DIRECTION_EAST,
+                self::DIRECTION_EAST => self::DIRECTION_NORTH
+            ];
+            $this->direction = $directions[$this->direction];
+            return $this;
+        }
+    
+        public function advance(): self {
+            switch ($this->direction) {
+                case self::DIRECTION_NORTH:
+                    $this->position[1]++;
+                    break;
+                case self::DIRECTION_EAST:
+                    $this->position[0]++;
+                    break;
+                case self::DIRECTION_SOUTH:
+                    $this->position[1]--;
+                    break;
+                case self::DIRECTION_WEST:
+                    $this->position[0]--;
+                    break;
+            }
+            return $this;
+        }
+    
+        public function instructions(string $inst): void {
+            for ($i = 0; $i < strlen($inst); $i++) {
+                switch ($inst[$i]) {
+                    case 'R':
+                        $this->turnRight();
+                        break;
+                    case 'L':
+                        $this->turnLeft();
+                        break;
+                    case 'A':
+                        $this->advance();
+                        break;
+                    default:
+                        throw new InvalidArgumentException();
+                        break;
+                }
             }
         }
-        sort($result);
-        return $result;
     }
-
-    public function studentsByGradeAlphabetical(): array {
-        $result = [];
-        $grades = array_unique(array_values($this->students));
-        sort($grades);
-
-        foreach ($grades as $grade) {
-            $result[$grade] = $this->grade($grade);
-        }
-
-        return $result;
-    }
-}
-
 
 ?>
