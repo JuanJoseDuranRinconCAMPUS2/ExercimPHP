1d0d2d3fd3b490ed837103d391c0dcecfbbb3c74
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-05-28 19:02:44 -0500

    feat: :sparkles: Eighteenth Exercise PHP
    performing the exercism "Largest Series Product" exercise

--- api.php
+++ api.php
@@ -2,46 +2,36 @@
     //values for example
     declare(strict_types=1);
 
-function encode(string $input): string
+    class Series
     {
-        $encoded = '';
-        $length = strlen($input);
-        $count = 1;
-
-        for ($i = 0; $i < $length; $i++) {
-            $currentChar = $input[$i];
-            $nextChar = ($i + 1 < $length) ? $input[$i + 1] : '';
-
-            if ($currentChar === $nextChar) {
-                $count++;
-            } else {
-                $encoded .= ($count > 1 ? $count : '') . $currentChar;
-                $count = 1;
-            }
+        private string $series;
+    
+        public function __construct(string $value)
+        {
+            $this->series = $value;
         }
-
-        return $encoded;
-    }
-
-function decode(string $input): string
-    {
-        $decoded = '';
-        $length = strlen($input);
-        $count = '';
-
-        for ($i = 0; $i < $length; $i++) {
-            $currentChar = $input[$i];
-
-            if (ctype_digit($currentChar)) {
-                $count .= $currentChar;
-            } else {
-                $repeatCount = ($count !== '' ? intval($count) : 1);
-                $decoded .= str_repeat($currentChar, $repeatCount);
-                $count = '';
+    
+        public function largestProduct(int $span): int
+        {
+            $seriesLength = strlen($this->series);
+    
+            if ($span > $seriesLength || $span < 0) {
+                throw new InvalidArgumentException("the span must be less than series");
             }
+    
+            if (!ctype_digit($this->series)) {
+                throw new InvalidArgumentException("the data must be a number");
+            }
+    
+            $maxInSeries = $seriesLength - $span + 1;
+            $result = 0;
+    
+            for ($i = 0; $i < $maxInSeries; $i++) {
+                $product = array_product(str_split(substr($this->series, $i, $span)));
+                $result = max($product, $result);
+            }
+    
+            return $result;
         }
-
-        return $decoded;
     }
-
 ?>
